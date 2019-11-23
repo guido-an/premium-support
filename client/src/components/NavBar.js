dd7edacfe347733d8d301d600d48520b1ab405a0
Guido <carucciguido@gmail.com>
2019-11-23 12:21:54 +0100

    header component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,34 +8,52 @@ export default function NavBar() {
   const [open, setVisible] = useState(false);
 
   return (
-    <header>
-      <div id="header-mobile-container">
-        <div>
-          <img className="logo" src={logo} alt="logo-vanilla-marketing" />
+    <section>
+      <header id="header-mobile">
+        <div id="header-mobile-container">
+          <div>
+            <img className="logo" src={logo} alt="logo-vanilla-marketing" />
+          </div>
+          <div id="burger-icon">
+            <HamburgerMenu
+              isOpen={open}
+              menuClicked={() => setVisible(!open)}
+              width={30}
+              height={18}
+              strokeWidth={3}
+              rotate={0}
+              color="black"
+              borderRadius={0}
+              animationDuration={0.1}
+            />
+          </div>
         </div>
-        <div id="burger-icon">
-          <HamburgerMenu
-            isOpen={open}
-            menuClicked={() => setVisible(!open)}
-            width={30}
-            height={18}
-            strokeWidth={3}
-            rotate={0}
-            color="black"
-            borderRadius={0}
-            animationDuration={0.1}
-          />
+        {open && (
+          <nav id="header-nav">
+            <ul>
+              <Link to="/tickets">I miei tickets</Link>
+              <Link to="/crea-ticket">Apri un ticket</Link>
+              <p id="logout-btn">Logout</p>
+            </ul>
+          </nav>
+        )}
+      </header>
+      <header id="header-desktop">
+        <div id="header-desktop-container">
+          <div>
+            <img className="logo" src={logo} alt="logo-vanilla-marketing" />
+          </div>
+          <div>
+            <nav id="header-nav">
+              <ul>
+                <Link to="/tickets">I miei tickets</Link>
+                <Link to="/crea-ticket">Apri un ticket</Link>
+              </ul>
+            </nav>
+          </div>
+          <div><p id="logout-btn">Logout</p></div>
         </div>
-      </div>
-      {open && (
-        <nav id="header-nav">
-          <ul>
-            <Link to="/tickets">I miei tickets</Link>
-            <Link to="/crea-ticket">Apri un ticket</Link>
-            <p id="logout-btn">Logout</p>
-          </ul>
-        </nav>
-      )}
-    </header>
+      </header>
+    </section>
   );
 }
