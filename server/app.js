e5b1a52cd03af550b066a5c4287861684d963c78
Guido <carucciguido@gmail.com>
2019-12-16 18:23:19 +0100

    heroku deploy

--- server/app.js
+++ server/app.js
@@ -40,8 +40,9 @@ app.use(cors({
     credentials: true 
 }));
 
+app.use(express.static(path.join(__dirname, "build")));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
   
 
---
+++ server/build/precache-manifest.40340b45f7fea1e6aedb0051e31a846f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c867f812519e912ba27497335decaba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2446dca5808ddc84579d",
+    "url": "/static/css/main.159cbe8a.chunk.css"
+  },
+  {
+    "revision": "f4806c4e52d6e1a2f41c",
+    "url": "/static/js/2.d7836512.chunk.js"
+  },
+  {
+    "revision": "2446dca5808ddc84579d",
+    "url": "/static/js/main.60a6cc5d.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "6ad8f5e8e62623990fa168b9d209a6d4",
+    "url": "/static/media/logo-vanilla-marketing.6ad8f5e8.png"
+  }
+]);
