28896bd92d94731ec5b9d6cd17207a7413cb6dda
Guido <carucciguido@gmail.com>
2019-12-16 18:57:50 +0100

    heroku depoloy

---
+++ server/build/precache-manifest.249e80cac6a1c0c3b57ecbc39ee9d21d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19608373414c263285e332be96369a53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6aaaff1547e65bb6e20",
+    "url": "/static/css/main.159cbe8a.chunk.css"
+  },
+  {
+    "revision": "f4806c4e52d6e1a2f41c",
+    "url": "/static/js/2.d7836512.chunk.js"
+  },
+  {
+    "revision": "f6aaaff1547e65bb6e20",
+    "url": "/static/js/main.7722b1dd.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "6ad8f5e8e62623990fa168b9d209a6d4",
+    "url": "/static/media/logo-vanilla-marketing.6ad8f5e8.png"
+  }
+]);
