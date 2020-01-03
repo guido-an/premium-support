a7d3216adaff66fd25eaeb644d34931545812e73
Guido <carucciguido@gmail.com>
2020-01-03 18:17:20 +0100

    home fixed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,8 +4,8 @@ import Login from '../components/Login';
 export default function Home({liftUserUp, history}) {
   return (
     <section>
-      <h1 style={{ textAlign: 'center'}} className="title-page">Premium support</h1>
+      <h1 style={{ textAlign: 'center' }} className="title-page">Premium support</h1>
       <Login liftUserUp={liftUserUp} history={history} />
     </section>
-  );
+  )
 }
